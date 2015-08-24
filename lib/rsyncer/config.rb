9e4612af9cc637c75814e2a721276d7ef009c068
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-08-24 19:39:10 +0300

    Parse JSON on Vagrantfile side to allow more flexible configurations

--- lib/rsyncer/config.rb
+++ lib/rsyncer/config.rb
@@ -2,34 +2,21 @@ module VagrantPlugins
   module Rsyncer
     class Config < Vagrant.plugin(2, :config)
 
-      attr_accessor :path
+      attr_accessor :settings
 
       def initialize
-        @path = UNSET_VALUE
+        @settings = UNSET_VALUE
       end
 
       def finalize!
-        # set defaults
+        # TODO: set defaults
       end
 
       def validate(machine)
         errors = _detected_errors
 
-        config_path = machine.config.rsyncer.path
-        if config_path == UNSET_VALUE
-          errors << "Config file not given."
-        else
-          begin
-            config_file = File.read(config_path)
-            config_json = JSON.parse(config_file)
-
-            puts config_json
-          rescue Errno::ENOENT
-            errors << "Config file '#{config_path}' not found."
-          rescue JSON::ParserError
-            errors << "Config file not valid JSON."
-          end
-        end
+        # TODO: do validations for machine.config.rsyncer.settings
+        # and append to errors (an array)
 
         { "rsyncer" => errors }
       end
