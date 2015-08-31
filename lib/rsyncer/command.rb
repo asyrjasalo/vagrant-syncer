fddc2826fc1f6a249d692380a32c63649fbc6d02
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-08-31 22:25:57 +0300

    Initial full rsync. Watch file changes using Ruby's Listen.

--- lib/rsyncer/command.rb
+++ lib/rsyncer/command.rb
@@ -1,109 +1,103 @@
 require Vagrant.source_root.join("plugins/synced_folders/rsync/helper")
+require 'listen'
 
 module Vagrant
   module Rsyncer
     class Command < Vagrant.plugin(2, :command)
 
       def self.synopsis
-        "does a full sync and starts the rsyncer monitor"
+        "does a full sync and watches the changed files to sync"
       end
 
       def execute
         with_target_vms do |machine|
-          opts = {}
-          opts[:hostpath] = machine.config.rsyncer.settings['paths']['source']
-          opts[:guestpath] = machine.config.rsyncer.settings['paths']['target']
-          opts[:owner] = machine.config.rsyncer.settings['target']['user']
-          opts[:group] = machine.config.rsyncer.settings['target']['group']
-          opts[:exclude] = machine.config.rsyncer.settings['excludes']
-          opts[:verbose] = machine.config.rsyncer.settings['verbose']
-          opts[:args] = machine.config.rsyncer.settings['args']['rsync']
-          opts[:args] <<  machine.config.rsyncer.settings['target']['permissions']
-          opts[:args] << '-z'
-
-          ssh_settings = machine.ssh_info
-          if ssh_settings
-            machine.ui.info("Doing initial full sync to get the guest up to date.")
-            VagrantPlugins::SyncedFolderRSync::RsyncHelper.rsync_single(
-              machine, ssh_settings, opts)
-          end
-
-
-          paths = [opts[:hostpath]]
-          @logger = machine.ui
-
-          ignores = []
-          paths.each do |path|
-            if opts
-              Array(opts[:exclude]).each do |pattern|
-                ignores << VagrantPlugins::SyncedFolderRSync::RsyncHelper.exclude_to_regexp(path, pattern.to_s)
-              end
-            end
-          end
-
-          latency = machine.config.rsyncer.settings['latency']
-
-          case RUBY_PLATFORM
-          when /darwin/
-            require_relative('listeners/fsevents')
-            Listeners::FSEvents.new(paths, ignores, latency, @logger, self.method(:callback)).run
-          when /linux/
-            require_relative('listeners/inotify')
-            Listeners::INotify.new(paths, ignores, latency, @logger, self.method(:callback)).run
-          when /cygwin|mswin|mingw|bccwin|wince|emx/
-            require_relative('listeners/wdm')
-            Listeners::WDM.new(paths, ignores, latency, @logger, self.method(:callback)).run
-          else
-            raise Errors::OSNotSupportedError
-          end
+          full_sync(machine)
+          watch(machine, method(:rsync_path))
         end
-
         0
       end
 
-      def callback(paths, ignores, modified)
-        @logger.info("Changes detected: #{modified.to_a.join(',')}")
-
-        tosync = []
-        paths.each do |hostpath, folders|
-          found = catch(:done) do
-            modified.each do |changed|
-              match = nil
-              ignores.each do |ignore|
-                next unless match.nil?
-                match = ignore.match(changed)
-              end
-
-              next unless match.nil?
-              throw :done, true if changed.start_with?(hostpath)
-            end
-
-            false
-          end
-
-          tosync << folders if found
-        end
-
-        # Sync all the folders that need to be synced
-        tosync.each do |folders|
-          folders.each do |opts|
-            ssh_info = opts[:machine].ssh_info
-            do_rsync(opts[:machine], ssh_info, opts[:opts]) if ssh_info
-          end
-        end
+      def rsync_path(machine, path_opts)
+        VagrantPlugins::SyncedFolderRSync::RsyncHelper.rsync_single(
+          machine,
+          machine.ssh_info,
+          path_opts
+        )
       end
 
       private
 
-        def do_rsync(machine, ssh_info, opts)
-          start_time = Time.new
-          VagrantPlugins::SyncedFolderRSync::RsyncHelper.rsync_single(machine, ssh_info, opts)
-          end_time = Time.new
-          machine.ui.info(I18n.t(
-            "vagrant_gatling_rsync.gatling_ran",
-            date: end_time.strftime(machine.config.gatling.time_format),
-            milliseconds: (end_time - start_time) * 1000))
+      def parse_path_opts(path)
+        path_opts = {}
+        path_opts[:guestpath] = path['target']['path']
+        path_opts[:owner] = path['target']['user']
+        path_opts[:group] = path['target']['group']
+        path_opts[:exclude] = path['source']['excludes']
+        path_opts[:args] = []
+
+        if path['target']['permissions']
+          if path['target']['permissions']['user']
+            path_opts[:args] << '--chmod=u=' + path['target']['permissions']['user']
+          end
+
+          if path['target']['permissions']['group']
+            path_opts[:args] << '--chmod=g=' + path['target']['permissions']['group']
+          end
+
+          if path['target']['permissions']['other']
+            path_opts[:args] << '--chmod=o=' + path['target']['permissions']['other']
+          end
         end
+
+        path_opts
+      end
+
+      def full_sync(machine)
+        machine.ui.info(I18n.t('rsyncer.info.started'))
+
+        verbose = machine.config.rsyncer.settings['verbose']
+        rsync_args = machine.config.rsyncer.settings['args']['rsync']
+
+        machine.config.rsyncer.settings['paths'].each do |path|
+          path_opts = {}
+          path_opts[:args] = rsync_args
+          path_opts[:hostpath] = File.expand_path(path['source']['path'], machine.env.root_path)
+          path_opts[:verbose] = verbose
+          path_opts = path_opts.merge(parse_path_opts(path))
+          rsync_path(machine, path_opts)
+        end
+      end
+
+      def watch(machine, callback)
+        machine.ui.info(I18n.t('rsyncer.info.watching'))
+
+        verbose = machine.config.rsyncer.settings['verbose']
+        rsync_args = machine.config.rsyncer.settings['args']['rsync']
+
+        machine.config.rsyncer.settings['paths'].each do |path|
+          next unless path['source']['watch']
+
+          hostpath = File.expand_path(path['source']['path'], machine.env.root_path)
+          path_opts = {}
+          path_opts[:args] = rsync_args
+          path_opts[:verbose] = verbose
+          path_opts = path_opts.merge(parse_path_opts(path))
+          watch_opts = {
+            latency: path['source']['latency']
+          }
+
+          listener = Listen.to(hostpath, watch_opts) do |modified, added, removed|
+            (modified + added + removed).map do |change|
+              path_opts[:hostpath] = File.expand_path(change, machine.env.root_path)
+              # TODO: format the hostpath for Vagrant RsyncHelper
+              #callback.call(machine, path_opts)
+            end
+          end
+          listener.start
+        end
+
+        sleep
+      end
 
     end
   end
