98955d33b03ad648cfc194c13d43c79dee337a75
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-09-22 17:40:14 +0300

    Do not autosync on up/reload/resume

--- lib/spindle/plugin.rb
+++ lib/spindle/plugin.rb
@@ -22,17 +22,17 @@ module Vagrant
         Vagrant::Spindle::Commands::Spin
       end
 
-      action_hook :initial, :machine_action_up do |hook|
-        hook.append Vagrant::Spindle::Actions::Up
-      end
+      #action_hook :initial, :machine_action_up do |hook|
+      #  hook.append Vagrant::Spindle::Actions::Up
+      #end
 
-      action_hook :initial, :machine_action_reload do |hook|
-        hook.append Vagrant::Spindle::Actions::Reload
-      end
+      #action_hook :initial, :machine_action_reload do |hook|
+      #  hook.append Vagrant::Spindle::Actions::Reload
+      #end
 
-      action_hook :initial, :machine_action_resume do |hook|
-        hook.append Vagrant::Spindle::Actions::Resume
-      end
+      #action_hook :initial, :machine_action_resume do |hook|
+      #  hook.append Vagrant::Spindle::Actions::Resume
+      #end
 
     end
   end
