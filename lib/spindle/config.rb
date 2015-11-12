bd5ad9c98b8247005fc74865270d3b73099b8af7
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-11-12 16:46:30 +0200

    Read JSON config on the plugin side. Add error checks.

--- lib/spindle/config.rb
+++ lib/spindle/config.rb
@@ -2,10 +2,24 @@ module Vagrant
   module Spindle
     class Config < Vagrant.plugin(2, :config)
 
-      attr_accessor :settings
+      attr_accessor :config, :settings
 
       def initialize
-        @settings = {}
+        @config = nil
+      end
+
+      def finalize!
+        unless @config
+          raise Vagrant::Errors::VagrantError.new,
+            "Config file is not defined in Vagrantfile."
+        end
+
+        begin
+          config_content = File.read(@config)
+          @settings = JSON.parse(config_content, symbolize_names: true)
+        rescue Errno::ENOENT, JSON::ParserError => error
+          raise Vagrant::Errors::VagrantError.new, error
+        end
       end
 
     end
