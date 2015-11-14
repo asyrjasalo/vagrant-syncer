f4095ce36405027dc95336fc48ee799eb96b6d22
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-11-15 01:06:09 +0200

    Settings sorted alphabetically. Tidied up indent.

--- lib/syncer/config.rb
+++ lib/syncer/config.rb
@@ -5,19 +5,19 @@ module Vagrant
       attr_accessor :continuous, :initial, :interval, :show_events, :ssh_args
 
       def initialize
-        @continuous = UNSET_VALUE
-        @initial = UNSET_VALUE
-        @interval = UNSET_VALUE
-        @ssh_args = UNSET_VALUE
+        @continuous  = UNSET_VALUE
+        @initial     = UNSET_VALUE
+        @interval    = UNSET_VALUE
         @show_events = UNSET_VALUE
+        @ssh_args    = UNSET_VALUE
       end
 
       def finalize!
         @continuous = true    if @continuous == UNSET_VALUE
         @initial = true       if @initial == UNSET_VALUE
         @interval = 0.1       if @interval == UNSET_VALUE
-        @ssh_args = nil       if @ssh_args == UNSET_VALUE
         @show_events = false  if @show_events == UNSET_VALUE
+        @ssh_args = nil       if @ssh_args == UNSET_VALUE
       end
 
     end
