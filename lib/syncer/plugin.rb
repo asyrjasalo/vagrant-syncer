cd7d2d02f5e37d1be3ee3572e49e1248f3b24dd4
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-11-15 23:35:15 +0200

    More clear description

--- lib/syncer/plugin.rb
+++ lib/syncer/plugin.rb
@@ -5,7 +5,7 @@ module Vagrant
       name "Syncer"
 
       description <<-DESC
-      Watches for changed files on the host and synchronizes them to the machine.
+      Watches for changed files on the host and rsyncs them to the machine.
       DESC
 
       config "syncer" do
