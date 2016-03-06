525ecc3f4b7f093ad341d0c55bd35dc1d2df8050
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-03-06 19:00:57 +0200

    Add Control* SSH args to defaults only if not on Windows

--- lib/syncer/config.rb
+++ lib/syncer/config.rb
@@ -15,14 +15,22 @@ module Vagrant
         @interval = 0.2          if @interval == UNSET_VALUE || @interval <= 0.2
         @run_on_startup = true   if @run_on_startup == UNSET_VALUE
         @show_events = false     if @show_events == UNSET_VALUE
-        @ssh_args = [
-          '-o StrictHostKeyChecking=no',
-          '-o IdentitiesOnly=true',
-          '-o UserKnownHostsFile=/dev/null',
-          '-o ControlMaster=auto',
-          "-o ControlPath=#{File.join(Dir.tmpdir, "ssh.#{rand(1000)}")}",
-          '-o ControlPersist=10m'
-        ] if @ssh_args == UNSET_VALUE
+
+        if @ssh_args = UNSET_VALUE
+          @ssh_args = [
+            '-o StrictHostKeyChecking=no',
+            '-o IdentitiesOnly=true',
+            '-o UserKnownHostsFile=/dev/null',
+          ]
+
+          unless Vagrant::Util::Platform.windows?
+            @ssh_args += [
+              '-o ControlMaster=auto',
+              "-o ControlPath=#{File.join(Dir.tmpdir, "ssh.#{rand(1000)}")}",
+              '-o ControlPersist=10m'
+            ]
+          end
+        end
       end
 
     end
